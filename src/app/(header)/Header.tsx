c8ed2b09c99eac2829d4f26f54dd87e286509967
Chad Fernandez <contact@chadfernandez.me>
2023-12-30 13:34:52 +0800

    feat: added partial homepage hero section and minor updates on header

--- src/app/(header)/Header.tsx
+++ src/app/(header)/Header.tsx
@@ -5,8 +5,11 @@ export default function Header() {
   return (
     <header className="sticky inset-x-0 top-0 mb-2 border-b bg-white/40 backdrop-blur-md">
       <div className="container py-3">
-        <Link href="/" className="font-bold">
-          chof64/ex
+        <Link href="/" className="group font-bold">
+          <div className="flex w-fit items-center justify-center gap-2 text-neutral-600 transition delay-75 duration-150 ease-in-out group-hover:text-neutral-800">
+            <div className="aspect-square h-4 rounded-full bg-neutral-600 transition delay-75 duration-150 ease-in-out group-hover:bg-neutral-800" />
+            chof64/ex
+          </div>
         </Link>
       </div>
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from "react";
+import Balancer from "react-wrap-balancer";
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <main>
+      <section className="my-32">
+        <div className="container">
+          <h1 className="typo-h mx-auto max-w-4xl text-center text-3xl font-extrabold text-neutral-800 lg:text-5xl">
+            <Balancer>
+              My little learning platform tinkering with new tools and packages.
+            </Balancer>
+          </h1>
+          <p className="typo-p mx-auto max-w-xl text-center text-lg text-neutral-600 lg:max-w-3xl lg:text-xl">
+            <Balancer>
+              Experiment with new tools, learning what works and what doesn't is
+              the purpose.
+            </Balancer>
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
